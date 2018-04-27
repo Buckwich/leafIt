18f585649e609d58654cc8e249f761122129db48
Buckwich <s.steinruecken@gmx.net>
2018-04-27 10:33:41 +0200

    preload fonts

--- js/deferCss.js
+++ js/deferCss.js
@@ -1,15 +1,15 @@
-/* First CSS File */
-var giftofspeed = document.createElement('link');
-giftofspeed.rel = 'stylesheet';
-giftofspeed.href = 'index.css';
-giftofspeed.type = 'text/css';
-var godefer = document.getElementsByTagName('link')[0];
-godefer.parentNode.insertBefore(giftofspeed, godefer);
+// /* First CSS File */
+// var giftofspeed = document.createElement('link');
+// giftofspeed.rel = 'stylesheet';
+// giftofspeed.href = 'index.css';
+// giftofspeed.type = 'text/css';
+// var godefer = document.getElementsByTagName('link')[0];
+// godefer.parentNode.insertBefore(giftofspeed, godefer);
 
-/* Second CSS File */
-var giftofspeed2 = document.createElement('link');
-giftofspeed2.rel = 'stylesheet';
-giftofspeed2.href = 'https://fonts.googleapis.com/css?family=Oswald|Roboto';
-giftofspeed2.type = 'text/css';
-var godefer2 = document.getElementsByTagName('link')[0];
-godefer2.parentNode.insertBefore(giftofspeed2, godefer2);
+// /* Second CSS File */
+// var giftofspeed2 = document.createElement('link');
+// giftofspeed2.rel = 'stylesheet';
+// giftofspeed2.href = 'https://fonts.googleapis.com/css?family=Oswald|Roboto';
+// giftofspeed2.type = 'text/css';
+// var godefer2 = document.getElementsByTagName('link')[0];
+// godefer2.parentNode.insertBefore(giftofspeed2, godefer2);
